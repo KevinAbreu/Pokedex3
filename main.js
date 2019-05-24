23f8c00c1d57666a115becfba9e01d32b621f1b5
Kevin Abreu <kevinrabreu@hotmail.com>
2019-05-24 13:50:43 -0400

    Let's see if I can get this to work

--- main.js
+++ main.js
@@ -4,36 +4,51 @@ let factText = document.querySelector('#factText')
 let numberInput = document.querySelector('#numberInput')
 // numberInput.addEventListener('click', getFactFetch);
 
-// function getFactAjax(){
-//   let number = numberInput.value;
-//   console.log(number);
-//   let xhr = new XMLHttpRequest()
-//   xhr.open('GET', `https://pokeapi.co/api/v2/pokemon/${number}/`)
-//   xhr.onload = function(){
-//     if(this.status == 200 && number != ''){
-//       console.log(this.responseText);
-//       fact.style.display = 'block'
-//       factText.innerText = this.responseText
-//     }
-//   }
-//   xhr.send()
-// }
-
-function getFactFetch(){
-    let number = numberInput.value;
-    console.log(number);
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
-    .then(response => response.text())
-    .then(data => {
-      if(number != ''){
-        fact.style.display = 'block'
-        let info = JSON.parse(data)
-        let ability = info["abilities"][1]["ability"]["name"]
-        factText.innerText = info
-        console.log(ability);
-
-      }
-    })
-    .catch(err => console.log(err))
+class Pokemon {
+  constructor(hp,atk,def){
+    // this.type1 = type1
+    // this.type2 = type2
+    // this.ability1 = ability1
+    // this.ability2 = ability2
+    // this.ability3 = ability3
+    this.hp = hp
+    this.atk = atk
+    this.def = def
+    // this.satk = satk
+    // this.sdef = sdef
+    // this.spe = spe
+  }
 }
+
+function getFactFetch(number) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     let info = JSON.parse(this.responseText)
+     factText.innerText = info
+     pokemon = new Pokemon(info["stats"][5]["stat"]["hp"],info["stats"][4]["stat"]["attack"],info["stats"][3]["stat"]["defense"])
+     console.log(info);
+   }
+  };
+  xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${number}/`, true);
+  xhttp.send();
+}
+
+//
+// function getFactFetch(number){
+//     // let number = numberInput.value;
+//     // console.log(number);
+//
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
+//     .then(response => response.text())
+//     .then(data => {
+//       if(this.status == 200){
+//         fact.style.display = 'block'
+//         let info = JSON.parse(data)
+//         factText.innerText = info
+//         console.log(data);
+//         pokemon = new Pokemon(info["stats"][5]["stat"]["hp"],info["stats"][4]["stat"]["attack"],info["stats"][3]["stat"]["defense"])
+//       }
+//     })
+//     .catch(err => console.log(err))
+// }
--- pokeapi.js
+++ pokeapi.js
@@ -1,11 +1,27 @@
-// class Pokemon {
-//   constructor(ability,hp,atk,def,spatk,spdef,spe) {
-//       this.ability = info["abilities"][1]["ability"]["name"]
-//       this.hp = info["abilities"][1]["ability"]["name"]
-//       this.atk = info["abilities"][1]["ability"]["name"]
-//       this.def = info["abilities"][1]["ability"]["name"]
-//       this.spatk = info["abilities"][1]["ability"]["name"]
-//       this.spdef = info["abilities"][1]["ability"]["name"]
-//       this.spe = info["abilities"][1]["ability"]["name"]
-//   }
-// }
+    type1 = info["types"][0]["type"]["name"]
+    type2 = info["types"][1]["type"]["name"]
+    ability1 = info["abilities"][0]["ability"]["name"]
+    ability2 = info["abilities"][1]["ability"]["name"]
+    ability3 = info["abilities"][2]["ability"]["name"]
+    spe = info["stats"][0]["stat"]["name"]
+    sdef = info["stats"][1]["stat"]["special-defense"]
+    satk = info["stats"][2]["stat"]["special-attack"]
+    def = info["stats"][3]["stat"]["defense"]
+    atk = info["stats"][4]["stat"]["attack"]
+    hp = info["stats"][5]["stat"]["hp"]
+
+class Pokemon {
+  constructor(hp,atk,def){
+    // this.type1 = type1
+    // this.type2 = type2
+    // this.ability1 = ability1
+    // this.ability2 = ability2
+    // this.ability3 = ability3
+    this.hp = hp
+    this.atk = atk
+    this.def = def
+    // this.satk = satk
+    // this.sdef = sdef
+    // this.spe = spe
+  }
+}
