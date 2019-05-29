2f12c57b3c635799090678f2cb67a830ebc241c8
Kevin Abreu <kevinrabreu@hotmail.com>
2019-05-29 13:39:37 -0400

    added string interpolation instead of using the plus symbol to concatenate everything

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function getFactFetch() {
      let info = JSON.parse(this.responseText)
      pokemon = new Pokemon(info["stats"][5]["base_stat"],info["stats"][4]["base_stat"],info["stats"][3]["base_stat"],info["stats"][2]["base_stat"],info["stats"][1]["base_stat"],info["stats"][0]["base_stat"] )
     fact.style.display = 'block'
-    let str = 'HP: '  + pokemon.hp + ' Attack: ' + pokemon.atk + 'Defense: ' + pokemon.def + ' Sp.Attack: ' + pokemon.satk + ' Sp.Defense: ' + pokemon.sdef + ' Speed: ' + pokemon.spe
+    let str = `HP:    ${pokemon.hp}   Attack:   ${pokemon.atk}  Defense:   ${pokemon.def}   Sp.Attack:   ${pokemon.satk}   Sp.Defense:   ${pokemon.sdef}   Speed:   ${pokemon.spe}`
     factText.innerText = str
      console.log(info);
    }
@@ -36,12 +36,6 @@ function getFactFetch() {
   xhttp.open("GET", `https://pokeapi.co/api/v2/pokemon/${number}/`, true);
   xhttp.send();
 }
-
-
-//
-// function getFactFetch(number){
-//     // let number = numberInput.value;
-//     // console.log(number);
 //
 //     fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
 //     .then(response => response.text())
